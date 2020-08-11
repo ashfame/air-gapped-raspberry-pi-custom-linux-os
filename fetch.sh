0a2a726dba19ed1ed07201d434338bd12be1b44d
Ashfame <mail@ashfame.com>
2020-08-11 15:58:23 +0530

    our own script to download extensions with their dependencies

---
+++ fetch.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# Repository to download from
+ADDR="http://repo.tinycorelinux.net"
+
+# Tinycore version
+TC="11.x"
+
+# Processor architecture, current options are  x86  x86_64  armv6  armv7
+ARCH="armv7"
+
+# Kernel version to download the appropriate extensions for it. Run "uname -r" on your piCore based linux OS to see kernel version
+KERNEL="4.19.81-piCore-v7"
+
+function download {
+	# ext name
+	ext="$1"
+
+	# ensure extension is non-empty
+	if [[ -z $ext ]]
+	then
+		return
+	fi
+
+	# ensure extension has a suffix .tcz
+	if [[ $ext != *.tcz ]]
+	then
+		echo "adding suffix to $ext"
+		ext=$ext".tcz"
+	fi
+
+	# replace KERNEL placeholder in extension name, if present
+	if [[ $ext == *KERNEL* ]]
+	then
+		ext=${ext/KERNEL/$KERNEL}
+	fi
+
+	echo "[$ext] Downloading.."
+
+	URL="$ADDR/$TC/$ARCH/tcz/$ext"
+
+	# bail if we already have this extension downloaded
+	if [[ -f $ext ]]
+	then
+		echo "Already exists, going to check dependencies tree now."
+	else
+		# download the extension
+		wget -q $URL > /dev/null 2>&1
+
+		# @TODO: show message if we couldn't download the extension or some error happened
+		if [ "$?" != "0" ]
+		then
+			echo "Err: Could not download $ext"
+		fi
+
+		# download md5 of extension
+		wget -q $URL".md5.txt" > /dev/null 2>&1
+
+		# verify md5sum of extension
+		md5sum -c $ext".md5.txt"
+		if [ "$?" != "0" ]
+		then
+			echo "Err: MD5 checksum failed"
+		fi
+	fi
+
+	# download dependencies
+	wget -q -O dep.txt "$URL.dep" 2>&1
+
+	if [ "$?" == 0 ]
+	then
+		echo "Downloading dependencies.."
+		cp dep.txt $ext".dep"
+		for ext in `cat dep.txt`
+		do
+			echo "Attempting to download $ext"
+			download $ext
+		done
+	fi
+}
+
+if [[ -z $1 ]]
+then
+	echo "Err: No extension name given."
+	exit 1
+fi
+
+# first invocation
+download $1
+
+# set file permissions
+chown 1001:50 *.tcz*
+
+# cleanup
+rm dep.txt
+
+# clean exit
+exit 0
